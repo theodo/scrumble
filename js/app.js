8ec154d362e74bdc3efe4da0217ef55ff4566c40
nicgirault <nic.girault@gmail.com>
2015-12-11 16:51:15 +0100

    Update 2015-12-11T15:51:15.719Z

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 'use strict';
 var app;
 
-app = angular.module('NotSoShitty', ['ng', 'ngResource', 'ngAnimate', 'ngMaterial', 'md.data.table', 'ui.router', 'app.templates', 'Parse', 'LocalStorageModule', 'satellizer', 'permission', 'trello-api-client', 'angular-google-gapi', 'NotSoShitty.login', 'NotSoShitty.settings', 'NotSoShitty.storage', 'NotSoShitty.bdc', 'NotSoShitty.common', 'NotSoShitty.daily-report', 'NotSoShitty.gmail-client', 'NotSoShitty.feedback']);
+app = angular.module('NotSoShitty', ['ng', 'ngResource', 'ngAnimate', 'ngMaterial', 'md.data.table', 'ui.router', 'app.templates', 'Parse', 'LocalStorageModule', 'satellizer', 'permission', 'trello-api-client', 'angular-google-gapi', 'angular-redactor', 'NotSoShitty.login', 'NotSoShitty.settings', 'NotSoShitty.storage', 'NotSoShitty.bdc', 'NotSoShitty.common', 'NotSoShitty.daily-report', 'NotSoShitty.gmail-client', 'NotSoShitty.feedback']);
+
+app.config(function(redactorOptions) {
+  return redactorOptions.buttons = ['formatting', '|', 'bold', 'italic'];
+});
 
 app.config(function($locationProvider, $urlRouterProvider, ParseProvider) {
   $locationProvider.hashPrefix('!');
@@ -223,10 +227,14 @@ angular.module('NotSoShitty.gmail-client').run(function(GApi, GAuth) {
 });
 
 angular.module('NotSoShitty.gmail-client').service('mailer', function($state, $rootScope, GAuth) {
+  var wrapHtmlContent;
+  wrapHtmlContent = function(content) {
+    return "<!DOCTYPE html>\n<head>\n  <!--[if gte mso 9]><xml>\n  <o:OfficeDocumentSettings>\n  <o:AllowPNG/>\n  <o:PixelsPerInch>96</o:PixelsPerInch>\n  </o:OfficeDocumentSettings>\n  </xml><![endif]-->\n  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n      <meta name=\"viewport\" content=\"width=device-width\">\n      <meta http-equiv=\"X-UA-Compatible\" content=\"IE=9; IE=8; IE=7; IE=EDGE\">\n      <title>Template Base</title>\n</head>\n<body style=\"width: 100% !important;min-width: 100%;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100% !important;margin: 0;padding: 0;background-color: #FFFFFF\">" + content + "</body>";
+  };
   return {
     send: function(message, callback) {
       return GAuth.checkAuth().then(function() {
-        var base64EncodedEmail, email, email_lines, now, request, user;
+        var base64EncodedEmail, content, email, email_lines, now, request, template, user;
         if (message.to == null) {
           return callback({
             message: "No 'to' field",
@@ -245,6 +253,8 @@ angular.module('NotSoShitty.gmail-client').service('mailer', function($state, $r
             code: 400
           });
         }
+        content = '<h1>coucou</h1><p><img class="center fullwidth" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block;border: none;height: auto;line-height: 100%;margin: 0 auto;float: none;width: 100% !important;max-width: 580px" align="center" border="0" data-custom-width="580" src="http://i.imgur.com/PS65fNY.png" alt="Image" title="Image"></p>';
+        template = "<!DOCTYPE html><head>\n<!--[if gte mso 9]><xml>\n<o:OfficeDocumentSettings>\n<o:AllowPNG/>\n<o:PixelsPerInch>96</o:PixelsPerInch>\n</o:OfficeDocumentSettings>\n</xml><![endif]-->\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n    <meta name=\"viewport\" content=\"width=device-width\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=9; IE=8; IE=7; IE=EDGE\">\n    <title>Template Base</title>\n</head>\n<body style=\"width: 100% !important;min-width: 100%;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100% !important;margin: 0;padding: 0;background-color: #FFFFFF\">\n<img src=\"http://vignette3.wikia.nocookie.net/nintendo/images/e/e2/Artwork_-_MARIO.svg/revision/latest/scale-to-width-down/443?cb=20150524030216&path-prefix=en\" alt=\"Image\" title=\"Image\">\n</body></html>";
         user = $rootScope.gapi.user;
         now = new Date();
         now = now.toString();
@@ -259,7 +269,7 @@ angular.module('NotSoShitty.gmail-client').service('mailer', function($state, $r
         email_lines.push("Subject: " + message.subject);
         email_lines.push("Date: " + now);
         email_lines.push('Message-ID: <1234@local.machine.example>');
-        email_lines.push("" + message.body);
+        email_lines.push(template);
         email = email_lines.join('\r\n').trim();
         base64EncodedEmail = btoa(email);
         base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
