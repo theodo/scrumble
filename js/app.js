75c6d04f06d3a4a12c73e4571eb40c81e1081c92
nicgirault <nic.girault@gmail.com>
2016-01-06 14:37:37 +0100

    Update 2016-01-06T13:37:37.116Z

--- js/app.js
+++ js/app.js
@@ -734,7 +734,7 @@ angular.module('NotSoShitty.login').config(function($authProvider) {
   return $authProvider.google({
     clientId: '605908567890-3bg3dmamghq5gd7i9sqsdhvoflef0qku.apps.googleusercontent.com',
     scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/gmail.send'],
-    redirectUri: window.location.origin + window.location.pathname,
+    redirectUrl: window.location.origin + window.location.pathname,
     responseType: 'token'
   });
 });
