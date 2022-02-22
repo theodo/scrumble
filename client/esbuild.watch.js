d056277be23587fe5cc6a10dfa31878e6cc56d9e
Kevin Raynel <kevinr@theodo.fr>
2022-02-22 09:30:51 +0100

    Do not minify ind ev

--- client/esbuild.watch.js
+++ client/esbuild.watch.js
@@ -1,10 +1,7 @@
 const config = require('./esbuild.config');
 
 require('esbuild')
-  .serve(
-    { servedir: 'public', port: 9876 },
-    { ...config, minify: true, minifyIdentifiers: false }
-  )
+  .serve({ servedir: 'public', port: 9876 }, config)
   .then((server) => {
     console.log('watching...', server.port);
   });
